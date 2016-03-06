5ead1f94d1492caafcf43babf91606bfcd05002d
Robin Tegg <robin@tegg.me.uk>
2016-03-06 17:55:18 +0000

    add check for no terminal to switch to sync based shell

--- src/main/java/com/robintegg/InteractiveShellRunner.java
+++ src/main/java/com/robintegg/InteractiveShellRunner.java
@@ -14,6 +14,7 @@ import org.crsh.console.jline.internal.Configuration;
 import org.crsh.plugin.PluginLifeCycle;
 import org.crsh.shell.Shell;
 import org.crsh.shell.ShellFactory;
+import org.crsh.shell.impl.command.CRaSHShellFactory;
 import org.crsh.util.InterruptHandler;
 import org.fusesource.jansi.AnsiConsole;
 import org.springframework.beans.factory.DisposableBean;
@@ -25,82 +26,92 @@ import org.springframework.stereotype.Component;
 //@Component
 public class InteractiveShellRunner implements CommandLineRunner, InitializingBean, DisposableBean {
 
-    final private PluginLifeCycle crshBootstrapBean;
-    private Shell shell;
-    private Terminal term;
+	final private PluginLifeCycle crshBootstrapBean;
+	private Shell shell;
+	private Terminal term;
 
-    @Autowired
-    public InteractiveShellRunner(PluginLifeCycle crshBootstrapBean) {
-        this.crshBootstrapBean = crshBootstrapBean;
-    }
+	@Autowired
+	public InteractiveShellRunner(PluginLifeCycle crshBootstrapBean) {
+		this.crshBootstrapBean = crshBootstrapBean;
+	}
 
-    @Override
-    public void afterPropertiesSet() throws Exception {
-        ShellFactory shellFactory = crshBootstrapBean.getContext().getPlugin(ShellFactory.class);
-        shell = shellFactory.create(null);
-    }
+	@Override
+	public void afterPropertiesSet() throws Exception {
+		CRaSHShellFactory shellFactory = (CRaSHShellFactory) crshBootstrapBean.getContext()
+				.getPlugin(ShellFactory.class);
 
-    @Override
-    public void destroy() throws Exception {
-        try {
-            if (term != null) {
-                term.restore();
-            }
-        } catch (Exception ignore) {
-        }
-    }
+		String terminal = System.getProperty("org.crsh.console.jline.terminal");
+		if (terminal.equals("none")) {
+			shell = shellFactory.create(null, false);
+		} else {
+			shell = shellFactory.create(null);
+		}
+	}
 
-    @Override
-    public void run(String... args) throws Exception {
+	@Override
+	public void destroy() throws Exception {
+		try {
+			if (term != null) {
+				term.restore();
+			}
+		} catch (Exception ignore) {
+		}
+	}
 
-        if (shell != null) {
+	@Override
+	public void run(String... args) throws Exception {
 
-            term = TerminalFactory.create();
+		if (shell != null) {
 
-            //
-            String encoding = Configuration.getEncoding();
+			term = TerminalFactory.create();
 
-            // Use AnsiConsole only if term doesnt support Ansi
-            PrintStream out;
-            PrintStream err;
-            boolean ansi;
-            if (term.isAnsiSupported()) {
-                out = new PrintStream(new BufferedOutputStream(term.wrapOutIfNeeded(new FileOutputStream(FileDescriptor.out)), 16384), false, encoding);
-                err = new PrintStream(new BufferedOutputStream(term.wrapOutIfNeeded(new FileOutputStream(FileDescriptor.err)), 16384), false, encoding);
-                ansi = true;
-            } else {
-                out = AnsiConsole.out;
-                err = AnsiConsole.err;
-                ansi = false;
-            }
+			//
+			String encoding = Configuration.getEncoding();
 
-            //
-            FileInputStream in = new FileInputStream(FileDescriptor.in);
-            ConsoleReader reader = new ConsoleReader(null, in, out, term);
+			// Use AnsiConsole only if term doesnt support Ansi
+			PrintStream out;
+			PrintStream err;
+			boolean ansi;
+			if (term.isAnsiSupported()) {
+				out = new PrintStream(
+						new BufferedOutputStream(term.wrapOutIfNeeded(new FileOutputStream(FileDescriptor.out)), 16384),
+						false, encoding);
+				err = new PrintStream(
+						new BufferedOutputStream(term.wrapOutIfNeeded(new FileOutputStream(FileDescriptor.err)), 16384),
+						false, encoding);
+				ansi = true;
+			} else {
+				out = AnsiConsole.out;
+				err = AnsiConsole.err;
+				ansi = false;
+			}
 
-            //
-            final JLineProcessor processor = new JLineProcessor(ansi, shell, reader, out);
+			//
+			FileInputStream in = new FileInputStream(FileDescriptor.in);
+			ConsoleReader reader = new ConsoleReader(null, in, out, term);
 
-            //
-            InterruptHandler interruptHandler = new InterruptHandler(processor::interrupt);
-            interruptHandler.install();
+			//
+			final JLineProcessor processor = new JLineProcessor(ansi, shell, reader, out);
 
-            //
-            Thread thread = new Thread(processor);
-            thread.setDaemon(true);
-            thread.start();
+			//
+			InterruptHandler interruptHandler = new InterruptHandler(processor::interrupt);
+			interruptHandler.install();
 
+			//
+			Thread thread = new Thread(processor);
+			thread.setDaemon(true);
+			thread.start();
 
-            try {
-                processor.closed();
-            } catch (Throwable t) {
-                t.printStackTrace();
-            }
-            
-            System.exit(0);
+			try {
+				processor.closed();
+			} catch (Throwable t) {
+				t.printStackTrace();
+			}
 
-        }
+			System.exit(0);
 
-    }
+		}
+
+	}
 
 }
